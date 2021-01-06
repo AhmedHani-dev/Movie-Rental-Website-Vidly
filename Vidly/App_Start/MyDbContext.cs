bb1f320a8d4463c62f967d661de6aa22573b6fed
AhmedHani-dev <ahany1111@gmail.com>
2021-01-07 00:48:38 +0200

    Add project files.

---
+++ Vidly/App_Start/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+	public class MyDbContext : DbContext
+	{
+		public DbSet<Customer> Customers { get; set; }
+	}
+}
---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+	public class CustomersController : Controller
+	{
+		List<Customer> customers = new List<Customer>
+		{
+			new Customer { Name = "John Smith", Id = 1},
+			new Customer { Name = "Mary Williams", Id = 2}
+		};
+		// GET: Customers
+		public ActionResult Index()
+		{
+
+			return View(customers);
+		}
+
+		[Route("Details/{Id}")]
+		public ActionResult Details(int id, string name)
+		{
+			foreach (var customer in customers)
+				if (customer.Id == id)
+					return View(customer);
+
+			return HttpNotFound();
+		}
+	}
+}
