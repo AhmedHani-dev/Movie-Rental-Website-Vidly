e61588f7fa5100f206ab1860e045a8987e201cd2
AhmedHani-dev <ahany1111@gmail.com>
2021-01-07 21:09:47 +0200

    Finished Exercise 1

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -24,6 +24,7 @@ namespace Vidly.Controllers
 		public ActionResult Index()
 		{
 			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+			//can be _context.Customers.Include("MemberShipType").ToList();
 			return View(customers);
 		}
 
