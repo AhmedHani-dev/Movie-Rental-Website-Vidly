3904d1d708973509f3e1c029ff3cf83827b6d5f4
AhmedHani-dev <ahany1111@gmail.com>
2021-01-07 19:48:20 +0200

    Querying Objects and Eager Loading

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,29 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.App_Start;
 using Vidly.Models;
 
 namespace Vidly.Controllers
 {
 	public class CustomersController : Controller
 	{
-		List<Customer> customers = new List<Customer>
+		private MyDbContext _context;
+		public CustomersController()
 		{
-			new Customer { Name = "John Smith", Id = 1},
-			new Customer { Name = "Mary Williams", Id = 2}
-		};
+			_context = new MyDbContext();
+		}
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
 		// GET: Customers
 		public ActionResult Index()
 		{
-
+			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 			return View(customers);
 		}
 
 		[Route("Details/{Id}")]
 		public ActionResult Details(int id, string name)
 		{
+			var customers = _context.Customers;
 			foreach (var customer in customers)
 				if (customer.Id == id)
 					return View(customer);
